3f30307e51b7c5e9289df0d18612a63cc0f57b4b
SuRu <33urus@gmail.com>
2021-10-09 12:17:15 +0300

    added Closures implementation

--- ex-21-minigrep/src/lib.rs
+++ ex-21-minigrep/src/lib.rs
@@ -16,14 +16,22 @@ impl Config {
             let query = args[1].clone();
             let file_name = args[2].clone();
 
-            Ok(Config { query, file_name, case_sensitive: true })
+            Ok(Config {
+                query,
+                file_name,
+                case_sensitive: true,
+            })
         } else if args.len() == 4 {
             let flag = args[1].clone();
             let query = args[2].clone();
             let file_name = args[3].clone();
 
             if flag.eq("-i") {
-                Ok(Config { query, file_name, case_sensitive: false })
+                Ok(Config {
+                    query,
+                    file_name,
+                    case_sensitive: false,
+                })
             } else {
                 Err(Config::ERR_MSG)
             }
@@ -33,8 +41,7 @@ impl Config {
     }
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 struct QueryResult {
     line: u32,
     text: String,
@@ -46,13 +53,12 @@ impl QueryResult {
     }
 }
 
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(&config.file_name)?;
     let results = if config.case_sensitive {
-        search(&config.query, &content)
+        search_v2(&config.query, &content)
     } else {
-        search_case_insensitive(&config.query, &content)
+        search_case_insensitive_v2(&config.query, &content)
     };
 
     results.iter().for_each(QueryResult::print);
@@ -64,9 +70,26 @@ fn search(query: &str, contents: &str) -> Vec<QueryResult> {
     let mut results: Vec<QueryResult> = Vec::new();
     for (ix, line) in contents.lines().enumerate() {
         if line.contains(query) {
-            results.push(QueryResult { line: (ix + 1) as u32, text: line.to_string() });
+            results.push(QueryResult {
+                line: (ix + 1) as u32,
+                text: line.to_string(),
+            });
         }
     }
+    results
+}
+
+fn search_v2(query: &str, contents: &str) -> Vec<QueryResult> {
+    let results: Vec<QueryResult> = contents
+        .lines()
+        .enumerate()
+        .filter(|(_ix, text)| text.contains(query))
+        .map(|(ix, text)| QueryResult {
+            line: (ix + 1) as u32,
+            text: text.to_string(),
+        })
+        .collect();
+
     results
 }
 
@@ -75,9 +98,27 @@ fn search_case_insensitive(query: &str, contents: &str) -> Vec<QueryResult> {
     let mut results: Vec<QueryResult> = Vec::new();
     for (ix, line) in contents.lines().enumerate() {
         if line.to_lowercase().contains(&query) {
-            results.push(QueryResult { line: (ix + 1) as u32, text: line.to_string() });
+            results.push(QueryResult {
+                line: (ix + 1) as u32,
+                text: line.to_string(),
+            });
         }
     }
+    results
+}
+
+fn search_case_insensitive_v2(query: &str, contents: &str) -> Vec<QueryResult> {
+    let query = query.to_lowercase();
+    let results: Vec<QueryResult> = contents
+        .lines()
+        .enumerate()
+        .filter(|(_ix, text)| text.to_lowercase().contains(&query))
+        .map(|(ix, text)| QueryResult {
+            line: (ix + 1) as u32,
+            text: text.to_string(),
+        })
+        .collect();
+
     results
 }
 
@@ -93,7 +134,10 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        let expected = vec![QueryResult { line: 2, text: "safe, fast, productive.".to_string() }];
+        let expected = vec![QueryResult {
+            line: 2,
+            text: "safe, fast, productive.".to_string(),
+        }];
 
         assert_eq!(expected, search(query, contents));
     }
@@ -106,7 +150,10 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        let expected = vec![QueryResult { line: 1, text: "Rust:".to_string() }];
+        let expected = vec![QueryResult {
+            line: 1,
+            text: "Rust:".to_string(),
+        }];
 
         assert_eq!(expected, search_case_insensitive(query, contents));
     }
